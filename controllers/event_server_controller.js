17467bacb8a6eae40e1b58e53ade362e1cd4ef47
galicians <galicians@gmail.com>
2014-12-22 19:10:09 +0000

    refactoring on the event controller

--- controllers/event_server_controller.js
+++ controllers/event_server_controller.js
@@ -1,31 +1,29 @@
-
 var mongoose = require('mongoose');
-
 var Model = require('../models/event');
-
 var EventModel = mongoose.model('Event');
+
+
+var sendBackData = function(res, err, data){
+   if(err){
+        res.send(500);
+    } else {
+        if(data){
+            res.send(200, data);
+        } else {
+            res.send(404);
+        }
+    }
+};
+
 
 exports.getAllEvents = function(req, res){
     EventModel.find(function(err,data){
-        if(err){
-            res.send(500);
-        }else{
-            res.send(200, data);
-        }
-        
+      sendBackData(res, err, data);      
     });
 };
 
 exports.findSingle = function(req, res){
     EventModel.findById(req.params.id, function(err, data){
-        if(err){
-            res.send(500);
-        } else {
-            if(data){
-                res.send(200, data);
-            } else {
-                res.send(404);
-            }
-        }
+        sendBackData(res, err, data);
     });
 };
