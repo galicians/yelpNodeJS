bbcd7dfd129de017e9042e70506484481ee5284f
galicians <galicians@gmail.com>
2014-12-22 01:29:16 +0000

    second test event suite passing

--- models/event.js
+++ models/event.js
@@ -5,4 +5,15 @@ var mongoose = require('mongoose'),
         ratings : []
 });
 
+EventSchema.pre('save', function(next){
+    var totalRatings = 0;
+
+    for(var i = 0; i < this.ratings.length; i++) {
+        totalRatings += this.ratings[i].rating;
+    }
+
+    this.averageRating = totalRatings / this.ratings.length;
+    next();
+});
+
 module.exports = mongoose.model('Event', EventSchema);
--- test/event_spec.js
+++ test/event_spec.js
@@ -2,10 +2,29 @@ var should = require('should');
 var Model = require('../models/event');
 
 describe("Event model", function() {
+
     it("should have an array of ratings", function(){
         var event = new Model();
         event.ratings.should.not.equal(undefined);
     });
 
-    
+    describe("when ssaving event", function(){
+        it("should calculate average rating", function(){
+            Model.prototype.save = function(callback){
+                callback();
+            };
+            var event = new Model({
+                ratings: [{
+                    rating: 1
+                }, {
+                    rating: 2
+                }]
+            });
+
+            event.save(function(){
+                event.averageRating.should.equal(1.5);
+            });
+        });
+    });
+
 });
