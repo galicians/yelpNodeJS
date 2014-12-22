5f382396640dc31ae47f91b38b29d62ed45a72d8
galicians <galicians@gmail.com>
2014-12-22 17:20:33 +0000

    refactoring on event model

--- models/event.js
+++ models/event.js
@@ -5,18 +5,28 @@ var mongoose = require('mongoose'),
         ratings : []
 });
 
-EventSchema.pre('save', function(next){
+EventSchema.methods.getTotalRating = function(){
     var totalRatings = 0;
 
     for(var i = 0; i < this.ratings.length; i++) {
         totalRatings += this.ratings[i].rating;
     }
 
+    return totalRatings;
+};
+
+EventSchema.methods.calculateAverageRating = function(){
+    var totalRatings = this.getTotalRating();
+
     if(this.ratings.length > 0) {
         this.averageRating = totalRatings / this.ratings.length;
     } else {
         this.averageRating = 0;
     }
+};
+
+EventSchema.pre('save', function(next){
+    this.calculateAverageRating();
 
     next();
 });
